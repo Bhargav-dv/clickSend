adf55e8a295ddc4a3e1cab4b9b01c070e2eacbf9
Bhargav Dharwad Venkata Ramana <bhargav.dharwadvenkataramana@aisydney.com.au>
2020-11-27 13:51:35 +1100

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+
+var api = require('./node_modules/clicksend/api.js');
+
+var smsApi = new api.SMSApi("SleepDrunk", "8661B131-FB76-9467-BD40-461086314F9C");
+var smsMessage = new api.SmsMessage();
+
+
+const app = express();
+
+
+//So that we can use HTML as file extention
+app.set("view engine", "html");
+app.engine("html", ejs.renderFile);
+
+//Public folder
+app.use(express.static(__dirname + '/public'));
+
+//midlerware for body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+//route
+app.get("/", (req,res)=>{
+    res.render("index");
+});
+
+//Post 
+app.post("/", (req, res)=>{
+    const number = req.body.num;
+    const message = req.body.text;
+    smsMessage.source = "sdk";
+    smsMessage.to = number;
+    smsMessage.body = message;  
+
+    var smsCollection = new api.SmsMessageCollection();
+    smsCollection.messages = [smsMessage];
+
+    smsApi.smsSendPost(smsCollection).then(function(response) {
+        console.log(response.body);
+      }).catch(function(err){
+        console.error(err.body);
+      });
+    // res.send(req.body);
+    // console.log(req.body)
+})
+
+app.listen(3200);
---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+//Client side JS
+
+const number = document.getElementById("number");
+const msg = document.getElementById("msg");
+const btn = document.getElementById("button");
+
+const sendMsg = () =>{
+    //non numberic char
+    const num = number.value;
+    const text = msg.value;
+    console.log(num)
+    fetch("/",{
+        method:"post",
+        headers:{
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({num:num, text:text})
+    })
+    .then((res)=>{
+        console.log(res);
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+};
+
+btn.addEventListener("click", sendMsg,false);
+
